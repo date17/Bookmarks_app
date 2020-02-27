c61e50b9a98275ea13e5bd69ac8786162455449e
date17 <date17@ezweb.ne.jp>
2020-02-27 12:42:17 +0900

    fixed layout of mail

--- resources/views/contact/mail.blade.php
+++ resources/views/contact/mail.blade.php
@@ -1,10 +1,10 @@
 お問い合わせを受け付けました<br>
 <br>
 ◦メールアドレス<br>
-{{!! $email !!}}<br>
+{{ $email }}<br>
 <br>
 ◦カテゴリ<br>
-{{!! $category !!}}<br>
+{{ $category }}<br>
 <br>
 ◦本文<br>
-{{!! nl2br($body) !!}}<br>
+{{ nl2br($body) }}<br>
