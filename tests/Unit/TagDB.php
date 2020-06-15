66310ef64ac6c28bad4185cd8d4d35f49d504efa
date17 <date17@ezweb.ne.jp>
2020-06-16 01:22:59 +0900

    fixed TagDB

--- tests/Unit/TagDB.php
+++ tests/Unit/TagDB.php
@@ -2,17 +2,44 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class TagDB extends TestCase
 {
+
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function testExample()
+    // public function testExample()
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    /**
+     * @test
+     */
+    public function register_Tagテーブル新規登録()
     {
-        $this->assertTrue(true);
+        $id = $this->user->id;
+        $tag = factory(Tag::class)->create([
+            "user_id" => $id
+        ]);
+
+        $this->assertDatabaseHas("tags", $tag->toArray());
     }
 }
